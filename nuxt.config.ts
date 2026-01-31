bfd9d571e7e3f2a4cca78cece2d6404b8c21cd1d
Hossein Ebrahimi <h.ebr141@outlook.com>
2026-01-31 18:37:13 +0330

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxt/ui"],
+
+  css: ["~/assets/css/main.css"],
+
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+})
