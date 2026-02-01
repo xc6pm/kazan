66e4019896621a2029fd352efd39ce0c995b183d
Hossein Ebrahimi <h.ebr141@outlook.com>
2026-02-01 10:50:04 +0330

    feat: add nuxt-i18n-micro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "nuxt-i18n-micro"],
+
+  i18n: {
+    locales: [{ code: "fa", iso: "fa-IR", dir: "rtl" }],
+    defaultLocale: "fa",
+    translationDir: "locales",
+    meta: true,
+    disablePageLocales: true,
+  },
 
   css: ["~/assets/css/main.css"],
 
